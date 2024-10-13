1f9247c46b04704da0219177dea58080fad1e9bd
Viktoria Markus <148903307+Viktoria1122@users.noreply.github.com>
2024-10-13 21:06:30 +0300

    Update Flower.java

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -7,13 +7,35 @@ import lombok.Setter;
 
 @Setter
 public class Flower {
-    @Getter
-    private double sepalLength;
     private FlowerColor color;
+    @Getter
+    private FlowerType flowerType;
     @Getter
     private double price;
     @Getter
-    private FlowerType flowerType;
+    private double sepallength;
+
+    public Flower(Flower flower) {
+        this.color = flower.color;
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepallength = flower.sepallength;
+    }
+
+    public Flower(double sepalLengthh, String colorr, double pricee) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public Flower() {
+        //TODO Auto-generated constructor stub
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException();
+        }
+        this.price = price;
+    }
 
     public String getColor() {
         return color.toString();
