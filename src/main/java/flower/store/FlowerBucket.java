3b300ba336986d6e6be3a6d5d1bbd169df51621e
Viktoria Markus <148903307+Viktoria1122@users.noreply.github.com>
2024-10-13 21:42:56 +0300

    Update FlowerBucket.java

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -3,20 +3,9 @@ import java.util.ArrayList;
 import java.util.List;
     
 public class FlowerBucket {
-    // private List<FlowerPack> flowerPacks;
-
-    // public FlowerBucket(List<FlowerPack> flowerPacks) {
-    //     this.flowerPacks = flowerPacks;
-    // }
-
-    // public FlowerBucket() {
-    //     //TODO Auto-generated constructor stub
-    // }
     private List<FlowerPack> flowerPacks = new ArrayList<>();
 
-    // Метод для додавання FlowerPack до букету
     public void add(FlowerPack flowerPack) {
-        // Додаємо FlowerPack до списку
         flowerPacks.add(flowerPack);
     }
 
